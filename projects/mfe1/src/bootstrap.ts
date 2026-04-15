5e6e325aca4e90e852ff9e7be217f30e061f183d
Oleh Kazban <oleh.kazban@globallogic.com>
2026-04-15 22:17:52 +0300

    Update

--- projects/mfe1/src/bootstrap.ts
+++ projects/mfe1/src/bootstrap.ts
@@ -1,11 +1,11 @@
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, provideZoneChangeDetection } from '@angular/core';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic().bootstrapModule(AppModule, { applicationProviders: [provideZoneChangeDetection()], })
   .catch(err => console.error(err));
